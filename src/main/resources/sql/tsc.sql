258ef64de0dc76a1599ecbde3724ab508f8c572f
guosukai <cole.guo@molimediagroup.com>
2019-07-16 18:49:56 +0800

    完成数据库sql

---
+++ src/main/resources/sql/tsc.sql
@@ -0,0 +1,78 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 50726
+ Source Host           : localhost:3306
+ Source Schema         : tsc
+
+ Target Server Type    : MySQL
+ Target Server Version : 50726
+ File Encoding         : 65001
+
+ Date: 16/07/2019 18:49:31
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for extract_config
+-- ----------------------------
+DROP TABLE IF EXISTS `extract_config`;
+CREATE TABLE `extract_config`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `from` smallint(1) NOT NULL COMMENT '提取来源  1url 2html 3 css',
+  `rule` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '提取规则',
+  `attribute` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '属性名称',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '属性提取配置' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for site
+-- ----------------------------
+DROP TABLE IF EXISTS `site`;
+CREATE TABLE `site`  (
+  `id` int(11) NOT NULL,
+  `charset` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '为空默认是utf-8',
+  `user_agent` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `time_out` smallint(6) NOT NULL DEFAULT 3000 COMMENT '超市时间 默认3秒',
+  `retry_time` smallint(6) NOT NULL DEFAULT 3 COMMENT '重试次数 默认3次',
+  `cycle_retry_time` smallint(6) NOT NULL DEFAULT 3 COMMENT '循环重试次数 默认3次',
+  `cookies` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'cookies 列表  用json存储',
+  `domain` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '域名',
+  `headers` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci COMMENT 'headers 用json存储',
+  `http_proxy` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT 'Http代理  如127.0.0.1:8080',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `update_time` timestamp(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for spider
+-- ----------------------------
+DROP TABLE IF EXISTS `spider`;
+CREATE TABLE `spider`  (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '名称',
+  `thread` smallint(1) NOT NULL DEFAULT 5 COMMENT '线程数 默认5',
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '初始的URL',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for task
+-- ----------------------------
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '任务名称',
+  `type` smallint(1) NOT NULL DEFAULT 1 COMMENT '1 爬虫 2数据清洗 ',
+  `thread` smallint(3) NOT NULL DEFAULT 5 COMMENT '线程数',
+  `create_time` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `uptime_time` timestamp(0) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
