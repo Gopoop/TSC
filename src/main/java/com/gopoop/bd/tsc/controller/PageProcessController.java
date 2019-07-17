14a2a96be5918f9bc3988447eb003a0840ad8dc7
guosukai <cole.guo@molimediagroup.com>
2019-07-17 19:11:41 +0800

    1

---
+++ src/main/java/com/gopoop/bd/tsc/controller/PageProcessController.java
@@ -0,0 +1,17 @@
+package com.gopoop.bd.tsc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 属性提取配置
+ * @author gopoop 2019-07-17
+ */
+@RestController
+@RequestMapping("/pageProcess")
+public class PageProcessController {
+
+
+
+}
+
