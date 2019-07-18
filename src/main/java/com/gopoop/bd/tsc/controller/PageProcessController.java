2efad9576cc4d031befef760c2321c98e636e818
guosukai <cole.guo@molimediagroup.com>
2019-07-18 11:33:29 +0800

    集成hakari连接池

--- src/main/java/com/gopoop/bd/tsc/controller/PageProcessController.java
+++ src/main/java/com/gopoop/bd/tsc/controller/PageProcessController.java
@@ -1,5 +1,8 @@
 package com.gopoop.bd.tsc.controller;
 
+import cn.hutool.json.JSONUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/pageProcess")
 public class PageProcessController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @RequestMapping("test")
+    public String test(){
+        return JSONUtil.toJsonStr(jdbcTemplate.queryForMap("SELECT * from page_process where id = 1"));
+    }
 
 }
 
